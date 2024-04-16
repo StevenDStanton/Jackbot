f7cb3153dcc8fc4da38bae14cc983d162d9e389a
Steven Stanton <steven.stanton@gmail.com>
2024-04-15 20:48:54 -0400

    Added test

--- handlers/discordMessageHandler.js
+++ handlers/discordMessageHandler.js
@@ -4,6 +4,10 @@ const jackbotRegex = new RegExp(process.env.BOT_NAME, 'i');
 
 async function handleDiscordMessage(message) {
   if (message.author.bot || !jackbotRegex.test(message.content)) return;
+  if (message.author.username === process.env.OWNER_NAME) {
+    console.log(message);
+    return;
+  }
   addMessageToHistory('user', message.author.username, message.content);
   const reply = await generateAIResponse();
   message.reply(reply);
