1dc1993a9be0ef4b15b24b76ecc88ca9b913fc76
Steven Stanton <steven.stanton@gmail.com>
2024-04-16 11:50:48 -0400

    Adding Turso DB

--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,7 @@ const { createClient } = require('@libsql/client');
 
 const client = createClient({
   url: process.env.TURSO_DATABASE_URL,
-  authToken: process.env.TURSO_AUTH_TOKEN,
+  authToken: process.env.TURSO_TOKEN,
 });
 
 async function insertChatRow(role, speaker, message) {
